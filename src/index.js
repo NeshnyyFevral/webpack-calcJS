76c29f00c9c0c4381b944ea455474b8e6ac1b23c
NeshnyyFevral <ivan.turovskiy.02@mail.ru>
2022-04-17 19:33:32 +0300

    move tab ivents in separate func

--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
 import './style/style.scss';
 import { render, calcField } from './script/render';
 import { buttons } from './script/buttons';
+import { tabIvents } from './script/tabIvents';
 
 const calc = document.querySelector('.calc');
 render(calc, buttons);
 
-calcField.addEventListener('click', () => {
-  const firstTabElem = calcField.children[0];
-  const lastTabElem = calcField.children[calcField.children.length - 1];
-
-  lastTabElem.onkeydown = (event) => {
-    if (event.key === 'Tab' && !event.shiftKey) {
-      firstTabElem.focus();
-      return false;
-    }
-    return null;
-  };
-  firstTabElem.onkeydown = (event) => {
-    if (event.key === 'Tab' && event.shiftKey) {
-      lastTabElem.focus();
-      return false;
-    }
-    return null;
-  };
-});
+calcField.addEventListener('keydown', () => tabIvents());
---
+++ src/script/tabIvents.js
@@ -0,0 +1,21 @@
+import { calcField } from './render';
+
+export function tabIvents() {
+  const firstTabElem = calcField.children[0];
+  const lastTabElem = calcField.children[calcField.children.length - 1];
+
+  lastTabElem.onkeydown = (event) => {
+    if (event.key === 'Tab' && !event.shiftKey) {
+      firstTabElem.focus();
+      return false;
+    }
+    return null;
+  };
+  firstTabElem.onkeydown = (event) => {
+    if (event.key === 'Tab' && event.shiftKey) {
+      lastTabElem.focus();
+      return false;
+    }
+    return null;
+  };
+}
